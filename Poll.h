97215a75fc0656bdeb9a6ea87e32bcf776d3d009
Elizaveta <lisa-bella97@mail.ru>
2016-05-03 12:10:54 +0300

    Update Poll.h

--- Poll.h
+++ Poll.h
@@ -15,13 +15,21 @@ private:
 	string _id; // уникальный идентификатор избирателя
 	string _name;
 	Poll* _poll;
-	Voter* _candidate;
+	Voter* _candidate; // кандидат, за которого проголосовал избиратель
+	int _votes; //кол-во голосов, отданных за кандидата (если избиратель - кандидат)
 public:
-	Voter(string id, string name, Poll* poll) : _id(id), _name(name), _poll(poll) { }
+	Voter(string id, string name, Poll* poll) : _id(id), _name(name), _poll(poll) { _candidate = nullptr; _votes = 0; }
 	string getID() const { return _id; }
 	string getName() const { return _name; }
 	Poll* getPoll() const { return _poll; }
+	Voter* getCandidate() const { return _candidate; }
+	int getVotes() const { return _votes; }
+	void increaseVotes() { _votes++; }
 	void changePoll(Poll* poll) { _poll = poll; }
+	void vote(Voter* candidate);
+	void showStatistics() const;
+	void print() const;
+	void removeCandidate() { _candidate = nullptr; }
 };
 
 class Poll // Избирательный участок
@@ -38,6 +46,8 @@ public:
 	void removeVoter(Voter* voter);
 	string getName() const { return _name; }
 	map<string, Voter*> getVoters() const { return _voters; }
+	void showStatistics() const;
+	void printVoters() const;
 	~Poll() { _voters.clear(); }
 };
 
