d7733651f335ed08af371db8682d4f1ec1719d3c
Elizaveta <lisa-bella97@mail.ru>
2016-05-05 00:05:30 +0300

    Update ElectSystem.h

--- ElectSystem.h
+++ ElectSystem.h
@@ -8,26 +8,23 @@
 class ElectSystem
 {
 private:
-	bool _state; // true - состояние выборов активировано
-	bool _elections; // true - начало выборов
+	enum State { usual, prepared, elections };
+	State _state; 
 	map<string, Poll*> _polls;
-	map<string, Voter*> _candidates;
+	map<string, Candidate*> _candidates;
 	map<string, Voter*> _voters;
 public:
-	ElectSystem(); 
-	ElectSystem(map<string, Poll*> polls, map<string, Voter*> candidates, bool state = false, bool elections = false)
-		: _polls(polls), _candidates(candidates), _state(state), _elections(elections) { }
+	ElectSystem() : _state(usual) { }
 	void createElections();
 	void startElections();
 	Poll* findPoll(string name) const;
 	Voter* findVoter(string id) const;
-	Voter* findCandidate(string id) const;
+	Candidate* findCandidate(string id) const;
 	void addVoter(string id, string name, string pollName);
-	void removeVoter(string id);
 	void addPoll(string name);
-	void removePoll(string name);
-	void merge(string source, string destination);
 	void addCandidate(string id);
+	void removeVoter(string id);
+	void merge(string source, string destination);
 	void removeCandidate(string id);
 	void vote(string idVoter, string idCandidate);
 	void printCandidates() const;
